cb7c7df150ffb5217ad790b152c8d6915a8c6a5f
thomassems <107775036+thomassems@users.noreply.github.com>
2026-01-17 21:02:20 -0500

    update api link

--- neuralbets/src/api/liveOddsApi.ts
+++ neuralbets/src/api/liveOddsApi.ts
@@ -1,6 +1,6 @@
 async function getLiveOdds() {
-    const response = await fetch('http://localhost:8082/bets/getdefaultodds');
-    // const response = await fetch('https://neuralbets-backend-663414512980.northamerica-northeast2.run.app');
+    // const response = await fetch('http://localhost:8082/bets/getdefaultodds');
+    const response = await fetch('https://neuralbets-backend-663414512980.northamerica-northeast2.run.app/bets/getdefaultodds');
     const data = await response.json();
     return data;
 }
